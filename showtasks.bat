29bf7b03105f55546f619854b75eeab5454ff355
jakubpanienski@wp.pl <jakubpanienski@wp.pl>
2022-03-10 03:42:15 +0100

    Add heroku configuration

--- showtasks.bat
+++ showtasks.bat
@@ -9,8 +9,8 @@ start chrome http://localhost:8080/crud/v1/tasks
 goto end
 
 :fail
-echo.0echo There were errors
+echo. echo There were errors
 
 :end
 echo.
-echo Work has finished
+echo Work has finished.
