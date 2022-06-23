2f38b53d538e3186fcd24e8dec233b33155d0fc2
kuba1306 <jakubpanienski@wp.pl>
2022-06-23 16:13:44 +0200

    32.2 ThyMyleaf

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,16 +1,18 @@
 package com.crud.tasks.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class StaticWebPageController {
 
     @RequestMapping("/")
-    public String index(Model model) { //Map<String, Object> model
-        model.addAttribute("variable", "My Thymeleaf variable");
-        model.addAttribute("two", 2);
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
         return "index";
     }
 }
